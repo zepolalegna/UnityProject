60c6894ffc73ccfdf18960adeb7213524155fdc9
Angela Lopez <damian15.AL63@gmail.com>
2022-10-09 17:41:51 -0600

    added buttoninput

---
+++ Unityproject1/Assets/Buttoninput.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ButtonInput : MonoBehaviour
+{
+    public Image graphic;
+    public Sprite standard;
+    public Sprite downgfx;
+    public Sprite upgfx;
+    public Sprite heldgfx;
+    public Text boolDisplay1;
+    public Text boolDisplay2;
+    public Text boolDisplay3;
+
+    void Start()
+    {
+        graphic.sprite = standard;    
+    }
+
+    void Update()
+    {
+        bool down = Input.GetButtonDown("Jump");
+        bool held = Input.GetButton("Jump");
+        bool up = Input.GetButtonUp("Jump");
+
+        if(down)
+        {
+            graphic.sprite = downgfx;
+        }
+        else if (held)
+        {
+            graphic.sprite = heldgfx;
+        }
+        else if (up)
+        {
+            graphic.sprite = upgfx;
+        }
+        else
+        {
+            graphic.sprite = standard;
+        }
+
+        boolDisplay1.text = " " + down;
+        boolDisplay2.text = " " + held;
+        boolDisplay3.text = " " + held;
+    }
+}
