62dd057051f014e0a210f349f850c2b24b2b2854
Angela Lopez <damian15.AL63@gmail.com>
2022-10-09 17:03:54 -0600

    added basic syntax

---
+++ Unityproject1/Assets/BasicSyntax.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using System.Collections;
+
+public class BasicSyntax : MonoBehaviour
+{
+    void Start ()
+    {
+        //this line is there to tell me the x position of my object
+
+        /*Hi there!
+         * this is two lines!
+         * */
+        Debug.Log(transform.position.x);
+        
+        if(transform.position.y <= 5f)
+        {
+            Debug.Log ("I'm about to hit the ground!");
+        }
+    }
+}
