4c83d2651efa3a47dfad28fc11c444f3dbad2bc5
Angela Lopez <damian15.AL63@gmail.com>
2022-10-09 17:40:01 -0600

    Added key input

---
+++ Unityproject1/Assets/KeyInput.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class KeyInput : MonoBehaviour
+{
+    public Image graphic;
+    public Sprite standard;
+    public Sprite downgfx;
+    public Sprite upgfx;
+    public Sprite heldgfx;
+    public Text boolDisplay1;
+    public Text boolDisplay2;
+    public Text boolDisplay3;
+
+    void Start()
+    {
+        graphic.sprite = standard;    
+    }
+
+    void Update()
+    {
+        bool down = Input.GetKeyDown(KeyCode.Space);
+        bool held = Input.GetKey(KeyCode.Space);
+        bool up = Input.GetKeyUp(KeyCode.Space);
+
+        if(down)
+        {
+            graphic.sprite = downgfx;
+        }
+        else if (held)
+        {
+            graphic.sprite = heldgfx;
+        }
+        else if (up)
+        {
+            graphic.sprite = upgfx;
+        }
+        else
+        {
+            graphic.sprite = standard;
+        }
+
+        boolDisplay1.text = " " + down;
+        boolDisplay2.text = " " + held;
+        boolDisplay3.text = " " + held;
+    }
+}
