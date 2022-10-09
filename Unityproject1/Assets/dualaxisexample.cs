43171baf293dd6599720dd89785f1b3400566b9a
Angela Lopez <damian15.AL63@gmail.com>
2022-10-09 17:45:18 -0600

    added dualaxisexample

---
+++ Unityproject1/Assets/dualaxisexample.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DualAxisExample : MonoBehaviour 
+{
+    public Text horizontalValueDisplayText;
+    public Text verticalValueDisplayText;
+    public float hRange;
+    public float vRange;
+    
+    void Update ()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        float xPos = h * hRange;
+        float yPos = v * vRange;
+        
+        transform.position = new Vector3(xPos, 0, vPos);
+        horizontalValueDisplayText.text = h.ToString("F2");
+        verticalValueDisplayText.text = v.ToString("F2");
+    }
+}
