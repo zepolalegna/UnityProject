449adb489b3f830f8d506b871bf326f1f9085ce2
Angela Lopez <damian15.AL63@gmail.com>
2022-11-26 14:20:33 -0700

    added code

--- UFO Defense Force/Assets/Scenes/Scripts/PlayerController.cs
+++ UFO Defense Force/Assets/Scenes/Scripts/PlayerController.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float horizontalInput;
+
+    public float speed;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,6 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        horizontalInput = Input.GetAxis("Horizontal");
     }
 }
