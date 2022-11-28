7eb2ef9da08a4916ced858ed3ed9a47dfb744041
Angela Lopez <damian15.AL63@gmail.com>
2022-11-27 17:06:00 -0700

    updated destroyoutofbounds code

--- UFO Defense Force/Assets/Scenes/Scripts/DestroyOutOfBounds.cs
+++ UFO Defense Force/Assets/Scenes/Scripts/DestroyOutOfBounds.cs
@@ -1,18 +1,32 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Destroy : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float topBounds = 30.0f;
+
+    public float lowerbounds = -10.0f;
+
+
+    private void Awake()
     {
-        
+        //Time.timeScale = 1;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (transform.position.z > topBounds)
+        {
+            Destroy(gameObject);
+        }
+        else if (transform.position.z < lowerbounds)
+        {
+            Debug.Log("Game Over");
+            Destroy(gameObject);
+            //Time.timeScale = 0;
+        }
     }
 }
