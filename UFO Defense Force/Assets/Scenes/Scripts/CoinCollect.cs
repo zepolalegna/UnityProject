55da96527579d1b0682e42c0d202936a2fad4fef
Angela Lopez <damian15.AL63@gmail.com>
2022-11-27 18:27:09 -0700

    Update CoinCollect.cs

--- UFO Defense Force/Assets/Scenes/Scripts/CoinCollect.cs
+++ UFO Defense Force/Assets/Scenes/Scripts/CoinCollect.cs
@@ -5,12 +5,17 @@ using UnityEngine;
 
 public class CoinCollect : MonoBehaviour
 {
+    public int coins;
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
+    public void OnTriggerEnter(Collider Col)
+    {
+        if(Col.gameObject.tag == "Coin")
+    }
     // Update is called once per frame
     void Update()
     {
