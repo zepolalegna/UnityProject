319e1473a8466df89e52d0f2d2a9d7aea59de2b7
Angela Lopez <damian15.AL63@gmail.com>
2022-11-29 12:29:15 -0700

    Update EnemySpawnManager.cs

--- UFO Defense Force/Assets/Scenes/Scripts/EnemySpawnManager.cs
+++ UFO Defense Force/Assets/Scenes/Scripts/EnemySpawnManager.cs
@@ -7,14 +7,14 @@ public class EnemySpawnManager : MonoBehaviour
     public GameObject[] ufoPrefabs; //Array to store UFO Ships
 
     public int ufoIndex;
-    
+
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.S))
-        { 
-            Instantiate(ufoPrefabs[ufoIndex], new Vector3(0, 0,15), ufoPrefabs[ufoIndex].transform.rotation);
+        {
+            Instantiate(ufoPrefabs[ufoIndex], new Vector3(0, 0, 15), ufoPrefabs[ufoIndex].transform.rotation);
         }
-      
+    }
 }
