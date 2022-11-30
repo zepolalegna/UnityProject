5eca8063d536f0ee042b6d79c2b2d094aa9ffae7
Angela Lopez <damian15.AL63@gmail.com>
2022-11-29 21:37:32 -0700

    updated codes and prefabs

--- UFO Defense Force/Assets/Scenes/Scripts/EnemySpawnManager.cs
+++ UFO Defense Force/Assets/Scenes/Scripts/EnemySpawnManager.cs
@@ -5,17 +5,17 @@ using UnityEngine;
 public class EnemySpawnManager : MonoBehaviour
 {
     public GameObject[] ufoPrefabs; //Array to store UFO Ships
-
-    public int ufoIndex;
-
+    private float spawnRangeX = 20f;
+    private float spawnPosZ = 20f;
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.S))
         {
+            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX,spawnRangeX),0,spawnPosZ);
             int ufoIndex = Random.Range(0,ufoPrefabs.Length); //Picks a random UFO on array
-            Instantiate(ufoPrefabs[ufoIndex], new Vector3(0, 0, 15), ufoPrefabs[ufoIndex].transform.rotation); //Spawns a UFO from the array
+            Instantiate(ufoPrefabs[ufoIndex],spawnPos, ufoPrefabs[ufoIndex].transform.rotation); //Spawns a UFO from the array
         }
     }
 }
