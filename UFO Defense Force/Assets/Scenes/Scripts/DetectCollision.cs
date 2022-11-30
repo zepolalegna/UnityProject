945d1a4fcc25210f004e1b5e55642b139950988d
Angela Lopez <damian15.AL63@gmail.com>
2022-11-29 22:42:21 -0700

    updated detect collision

--- UFO Defense Force/Assets/Scenes/Scripts/DetectCollision.cs
+++ UFO Defense Force/Assets/Scenes/Scripts/DetectCollision.cs
@@ -3,11 +3,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CollisionDetect : MonoBehaviour
+public class DetectCollision : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
-        Destroy(gameObject); //Destroy this game object
-        Destroy(other.gameObject);
+        Destroy(gameObject); //Destroy this game object 
+        Destroy(other.gameObject); //Destroys other game abject
     }
 }
