fad1236d2738371a86e03a79ca6c792f24576fe5
Angela Lopez <damian15.AL63@gmail.com>
2022-12-07 12:13:58 -0700

    fixed scoremanager code

--- UFO Defense Force/Assets/Scenes/Scripts/ScoreManager.cs
+++ UFO Defense Force/Assets/Scenes/Scripts/ScoreManager.cs
@@ -1,29 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
+using TMPro; // Text mesh pro name spaced added to access the libraries for UI
 
 public class ScoreManager : MonoBehaviour
 {
-    public int score;
 
-    public TextMeshProUGUI scoreText;
+    public int score; //keep our score value
 
-    public void IncreaseScore(int amount)
+    public TextMeshProUGUI scoreText; //Visual text element to be modified
+
+    public void IncreaseScore(int amount) //This method when called increases the score by a predetermined amount set in score variable
     {
         score += amount;
         UpdateScoreText();
-
+        
     }
 
-    public void DecreaseScore(int amount)
+    public void DecreaseScore(int amount) //This method when called increase the score by a predetermined amount set in score variable
     {
         score -= amount;
         UpdateScoreText();
     }
 
-    public void UpdateScoreText()
+    public void UpdateScoreText() //This method updates the score in the HUD UI text
     {
         scoreText.text = "Score: " + score;
     }
-}    
+}
