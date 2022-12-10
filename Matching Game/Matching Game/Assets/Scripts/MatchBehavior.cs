7b8d8997d7a6ea3b4a7fcef752cb8e9360e18463
Angela Lopez <damian15.AL63@gmail.com>
2022-12-10 11:32:03 -0700

    Update MatchBehavior.cs

--- Matching Game/Matching Game/Assets/Scripts/MatchBehavior.cs
+++ Matching Game/Matching Game/Assets/Scripts/MatchBehavior.cs
@@ -6,12 +6,16 @@ using UnityEngine;
 public class MatchBehavior : MonoBehaviour
 {
     public ID idObj;
+
     private void OnTriggerEnter(Collider other)
     {
-        var otherID = other.GetComponent<IDContainerBehavior>().idObj;
-        if (otherID == idObj)
-        {
-            Debug.Log("Matched");
+        if (other != null)
+        { 
+            var otherID = other.GetComponent<IDContainerBehavior>().idObj;
+            if (otherID == idObj)
+            {
+                Debug.Log("Matched");
+            }
         }
     }
 }
