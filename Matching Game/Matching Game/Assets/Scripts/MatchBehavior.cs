6a872180f8e4cca38cbebb8b891ebc11060107b9
Angela Lopez <damian15.AL63@gmail.com>
2022-12-10 11:00:42 -0700

    Update MatchBehavior.cs

--- Matching Game/Matching Game/Assets/Scripts/MatchBehavior.cs
+++ Matching Game/Matching Game/Assets/Scripts/MatchBehavior.cs
@@ -1,18 +1,13 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MatchBehavior : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public ID idObj;
+    private void OnTriggerEnter(Collider other)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+       Debug.Log(idObj); 
     }
 }
