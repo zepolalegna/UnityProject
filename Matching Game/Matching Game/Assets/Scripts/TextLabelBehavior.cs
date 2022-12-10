1dc4ae2e3197ba5f79ddbfc2aebffd6119bab16e
Angela Lopez <damian15.AL63@gmail.com>
2022-12-10 16:07:54 -0700

    Update TextLabelBehavior.cs

--- Matching Game/Matching Game/Assets/Scripts/TextLabelBehavior.cs
+++ Matching Game/Matching Game/Assets/Scripts/TextLabelBehavior.cs
@@ -2,21 +2,27 @@ using System;
 using System.Globalization;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Events;
 
 [RequireComponent(typeof(Text))]
 public class TextLabelBehavior : MonoBehaviour
 {
     public Text label;
-    public FloatData dataObj;
+    public UnityEvent startEvent;
 
     private void Start()
     {
         label = GetComponent<Text>();
-        UpdateLabel();
+        startEvent.Invoke();
     }
 
-    public void UpdateLabel()
+    public void UpdateLabel(FloatData obj)
     {
-        label.text = dataObj.value.ToString(CultureInfo.InvariantCulture);
+        label.text = obj.value.ToString(CultureInfo.InvariantCulture);
+    }
+
+    public void UpdateLabel(IntData obj)
+    {
+        label.text = obj.value.ToString(CultureInfo.InvariantCulture);
     }
 }
