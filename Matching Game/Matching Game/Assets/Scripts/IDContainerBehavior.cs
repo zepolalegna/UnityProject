60b9ea73f1ba197138ebb86c651adcf5e46cd618
Angela Lopez <damian15.AL63@gmail.com>
2022-12-10 12:55:32 -0700

    Update IDContainerBehavior.cs

--- Matching Game/Matching Game/Assets/Scripts/IDContainerBehavior.cs
+++ Matching Game/Matching Game/Assets/Scripts/IDContainerBehavior.cs
@@ -1,6 +1,13 @@
+using System;
 using UnityEngine;
-
+using UnityEngine.Events;
 public class IDContainerBehavior : MonoBehaviour
 {
    public ID idObj;
+   public UnityEvent startEvent;
+
+   public void Start()
+   {
+      startEvent.Invoke();
+   }
 }
