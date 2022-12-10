946164f02563c74bdfeaf0b544c3f130800d5640
Angela Lopez <damian15.AL63@gmail.com>
2022-12-10 16:51:19 -0700

    Update CoroutineBehavior.cs

--- Matching Game/Matching Game/Assets/Scripts/CoroutineBehavior.cs
+++ Matching Game/Matching Game/Assets/Scripts/CoroutineBehavior.cs
@@ -7,7 +7,7 @@ public class CoroutineBehavior : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Start");
     }
 
     // Update is called once per frame
