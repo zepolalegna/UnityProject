8cc03cbfa747faf3e5fb345ada41feeb1d8ac04e
Angela Lopez <damian15.AL63@gmail.com>
2022-12-10 16:16:22 -0700

    Update IntData.cs

--- Matching Game/Matching Game/Assets/Scripts/IntData.cs
+++ Matching Game/Matching Game/Assets/Scripts/IntData.cs
@@ -5,6 +5,11 @@ public class IntData : ScriptableObject
 {
     public int value;
 
+    public void SetValue(int number)
+    {
+        value = number;
+    }
+
     public void UpdateValue(int number)
     {
         value += number;
