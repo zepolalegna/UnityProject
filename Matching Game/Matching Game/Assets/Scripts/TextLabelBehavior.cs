f2e45453ca4e9f18fb12acad97e4abf3eaff04e4
Angela Lopez <damian15.AL63@gmail.com>
2022-12-10 15:06:02 -0700

    created TextLabelBehavior

---
+++ Matching Game/Matching Game/Assets/Scripts/TextLabelBehavior.cs
@@ -0,0 +1,16 @@
+using System.Globalization;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class TextLabelBehaviour : MonoBehaviour
+{
+    public Text label;
+    public FloatData dataObj;
+    private void Start()
+    {
+        label = GetComponent<Text>();
+        label.text = dataObj.value.ToString(CultureInfo.InvariantCulture);
+    }
+   
+}
