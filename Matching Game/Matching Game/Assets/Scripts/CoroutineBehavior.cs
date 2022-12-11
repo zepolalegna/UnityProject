f2b43529113d0fd325b2784e74589e0c89abdb56
Angela Lopez <damian15.AL63@gmail.com>
2022-12-10 17:17:20 -0700

    Update CoroutineBehavior.cs

--- Matching Game/Matching Game/Assets/Scripts/CoroutineBehavior.cs
+++ Matching Game/Matching Game/Assets/Scripts/CoroutineBehavior.cs
@@ -7,15 +7,17 @@ public class CoroutineBehavior : MonoBehaviour
     public bool canRun = true;
     public float seconds = 3.0f;
     private WaitForSeconds wfsObj;
+    private WaitForFixedUpdate wffuObj;
     IEnumerator Start()
     {
         wfsObj = new WaitForSeconds(seconds);
+        wffuObj = new WaitForFixedUpdate();
         Debug.Log("Start");
         yield return wfsObj;
         Debug.Log("Late Start");
         while (canRun)
         {
-            yield return new WaitForFixedUpdate();
+            yield return wffuObj;
             Debug.Log("Run on Start");
         }
     }
