e11fb49f8e46a295108b359a5778dd44449ceafe
Angela Lopez <damian15.AL63@gmail.com>
2022-12-10 17:50:05 -0700

    Update CoroutineBehavior.cs

--- Matching Game/Matching Game/Assets/Scripts/CoroutineBehavior.cs
+++ Matching Game/Matching Game/Assets/Scripts/CoroutineBehavior.cs
@@ -20,6 +20,7 @@ public class CoroutineBehavior : MonoBehaviour
         {
             yield return wfsObj;
             repeatEvent.Invoke();
+            Debug.Log(counterNum);
         }
         
     }
