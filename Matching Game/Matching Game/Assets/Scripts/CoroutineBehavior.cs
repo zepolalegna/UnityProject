7f8d69c970773b79e95665f7fd38d1123dd494bf
Angela Lopez <damian15.AL63@gmail.com>
2022-12-10 17:05:24 -0700

    Update CoroutineBehavior.cs

--- Matching Game/Matching Game/Assets/Scripts/CoroutineBehavior.cs
+++ Matching Game/Matching Game/Assets/Scripts/CoroutineBehavior.cs
@@ -5,22 +5,15 @@ using UnityEngine;
 public class CoroutineBehavior : MonoBehaviour
 {
     public bool canRun = true;
-    // Start is called before the first frame update
     IEnumerator Start()
     {
         Debug.Log("Start");
         yield return new WaitForSeconds(3);
         Debug.Log("Late Start");
-        while (true)
+        while (canRun)
         {
             yield return new WaitForFixedUpdate();
             Debug.Log("Run on Start");
         }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        Debug.Log("Run");
     }
 }
